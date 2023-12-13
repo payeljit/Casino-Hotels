dfc08834d5f45a1b61948fb231aded740147cb43
Payeljit <payelbanerjee9@yahoo.com>
2023-12-13 11:42:11 +0000

    Update about us section to adjust the image section to target different device display

--- wp-content/themes/casinohotel/template-parts/content-about.php
+++ wp-content/themes/casinohotel/template-parts/content-about.php
@@ -1,25 +1,51 @@
  <!-- About Us -->
  <div class="row p-5">
-      <div class="col-12 col-md-6"> 
-         <h6 class="text-pink mb-4"><?php echo get_field('content_title'); ?></h6>
-         <h2 class="mb-4"><?php echo get_field('content_heading'); ?></h2>
+   <?php
+   $content_title = get_field('content_title');
+   $content_heading = get_field('content_heading');
+   $section_image = get_field('section_image');
+
+   ?>
+   <!-- ======================= Column first ================== -->
+      <div class="col-12 col-md-6 "> 
+         <!-- Title -->
+         <h6 class="text-pink mb-4"><?php echo $content_title ; ?></h6>
+         <!-- Heading -->
+         <h2 class="mb-4"><?php echo $content_heading; ?></h2>
+
+         <!-- MOBILE ONLY -->
+         <div class="section-img  d-block d-md-none">
+            <?php 
+               if( !empty($section_image) ): ?>
+               <img class="w-100 float-en mb-4" src="<?php echo esc_url($section_image['url']); ?>" alt="<?php echo esc_attr($section_image['alt']); ?>" />
+               <?php
+               endif; 
+            ?>
+         </div>
+         <!-- .MOBILE ONLY -->
+
+         <!-- Content -->
          <?php the_field('content'); ?>
+         <!-- Button -->
          <a class="btn p-btn bg-pink btn-lg mt-4" href="#!" role="button">
            Explore Today 
            <span><img class="btn-arrow" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/arrow-right.png"></span>
          </a>
       </div>
 
+      <!-- ======================  Column second ================== -->
       <div class="col-12 col-md-6 section-img">
-         <?php 
-            $section_image = get_field('section_image');
-            if( !empty($section_image) ): ?>
-            <img class="w-100 float-end" src="<?php echo esc_url($section_image['url']); ?>" alt="<?php echo esc_attr($section_image['alt']); ?>" />
-            <?php
-             endif; 
-         ?>
+         <!-- Don't show this section on mobile version, show on medium screen and onwords -->
+         <div class="section-img d-none d-md-block">
+            <?php 
+               if( !empty($section_image) ): ?>
+               <img class="w-100 float-end" src="<?php echo esc_url($section_image['url']); ?>" alt="<?php echo esc_attr($section_image['alt']); ?>" />
+               <?php
+               endif; 
+            ?>
+         </div>
       </div>
-   </div>
+
+     </div>
  
-   </div>
    <!-- .About Us -->
