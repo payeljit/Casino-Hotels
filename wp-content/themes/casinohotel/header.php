2efefdfd13a76d0fce5fef461a195e38e32c2868
Payeljit <payelbanerjee9@yahoo.com>
2023-12-14 13:11:08 +0000

    Move header in a separate file

--- wp-content/themes/casinohotel/header.php
+++ wp-content/themes/casinohotel/header.php
@@ -13,35 +13,7 @@
 
 <body>
     <header>
-
-        <div class="container-fluid p-0">
-            <!-- Jumbotron -->
-            <?php
-    if(function_exists('the_custom_logo')) {
-        $custom_logo_id =   get_theme_mod('custom_logo');
-        $logo = wp_get_attachment_image_src($custom_logo_id);
-    }
-   $hero_image = get_field('hero_image'); ?>
-
-            <div class="p-5 w-100 bg-image h-200" style="
-    height: 636px;
-    background-image: 
-    url('<?php echo esc_url($hero_image['url']); ?>" alt="<?php echo esc_attr($hero_image['alt']); ?>');">
-
-                <div class="row text-center text-md-start">
-                    <div class="col-12 col-md-6 text-white" data-aos="fade-right">
-                        <img class="mb-4" src="<?php echo $logo[0]; ?>">
-                        <h1 class="mb-4"><?php echo get_field('hero_heading') ?></h1>
-                        <h5 class="mb-4"><?php echo get_field('hero_sub_heading')?></h5>
-                        <a class="btn p-btn bg-pink btn-lg" href="#table" role="button">
-                            Explore Today
-                            <span><img class="btn-arrow"
-                                    src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/arrow-down.png"></span>
-                        </a>
-                    </div>
-                </div>
-
-            </div>
-            <!-- .Jumbotron -->
+        <!-- ======================  Top banner section ========================  -->
+        <?php get_template_part('template-parts/content', 'banner')?>
 
     </header>
---
+++ wp-content/themes/casinohotel/template-parts/content-banner.php
@@ -0,0 +1,32 @@
+ <div class="container-fluid p-0">
+     <!-- I have added the custom logo function, so user can change the logo from admin area -->
+     <?php
+    if(function_exists('the_custom_logo')) {
+        $custom_logo_id =   get_theme_mod('custom_logo');
+        $logo = wp_get_attachment_image_src($custom_logo_id);
+    }
+    // banner image
+   $hero_image = get_field('hero_image'); 
+   $hero_heading = get_field('hero_heading');
+   $hero_sub_heading = get_field('hero_sub_heading');
+   ?>
+
+     <div class="p-5 w-100 bg-image h-200" style="
+    height: 636px;
+    background-image: 
+    url('<?php echo esc_url($hero_image['url']); ?>" alt="<?php echo esc_attr($hero_image['alt']); ?>');">
+
+         <div class="row text-center text-md-start">
+             <div class="col-12 col-md-6 text-white" data-aos="fade-right">
+                 <a href="<?php echo home_url();?>"><img class="mb-4" src="<?php echo $logo[0]; ?>"></a>
+                 <h1 class="mb-4"><?php echo $hero_heading;?></h1>
+                 <h5 class="mb-4"><?php echo $hero_sub_heading;?></h5>
+                 <a class="btn p-btn bg-pink btn-lg" href="#table" role="button">
+                     Explore Today
+                     <span><img class="btn-arrow"
+                             src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/arrow-down.png"></span>
+                 </a>
+             </div>
+         </div>
+
+     </div>
